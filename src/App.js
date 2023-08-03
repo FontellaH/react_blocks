2bf29c52598c4886cefdfa78c2023ff79eb8505b
Fontella Hooper <hooperfontella6@gmail.com>
2023-08-03 15:40:52 -0700

    react_blocks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Main from './components/Main';
+import SubContent from './components/Subcontent';
+import Advertisement from './components/Advertisment';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Navigation />
+      <Main>
+        <SubContent />
+        <SubContent />
+        <SubContent />
+        <Advertisement />
+      </Main>         
     </div>
   );
 }
